fa2b94f3a225170a14c3322eddc123396bd66767
Matías González Pazos <matiasggonzalez42@gmail.com>
2025-08-10 12:09:10 +0200

    Implement API client to fetch actual data

--- app/services/pokebase_client.py
+++ app/services/pokebase_client.py
@@ -1,25 +1,54 @@
+import asyncio
 from typing import List
-from app.services.schemas import Berry
+
+import httpx
+
+from app.services.schemas import Berry, NamedAPIResource, NamedAPIResourceList
 
 
 class PokebaseClient:
+    ENDPOINT: str = ""
+    API_VERSION: str = "v2"
+
     def __init__(self, base_url: str):
         self.base_url = base_url
+        self.path = f"{self.base_url}/{self.API_VERSION}/{self.ENDPOINT}"
 
 
-class BerryClient(PokebaseClient):
-    def get_all_details(self) -> List[Berry]:
-        # TODO: Implement the actual API call to fetch berry details.
-        return [
-            Berry(
-                id=1,
-                name="cheri",
-                growth_time=3,
-                max_harvest=5,
-                natural_gift_power=50,
-                size=20,
-                smoothness=10,
-                soil_dryness=5,
-            ),
-        ]
-        
+class NamedAssetAPIResource[AssetDetailT](PokebaseClient):
+    DETAIL_CLASS: type[AssetDetailT]
+
+    def __init__(self, base_url: str):
+        super().__init__(base_url)
+        self.client = httpx.AsyncClient()
+
+    async def get_list(self) -> List[NamedAPIResource]:
+        next_url = self.path
+        all_resources = []
+
+        while next_url:
+            resource_list = await self.get_page(next_url)
+            all_resources.extend(resource_list.results)
+            next_url = resource_list.next
+
+        return [NamedAPIResource(**item) for item in all_resources]
+
+    async def get_page(self, url: str) -> NamedAPIResourceList:
+        response = await self.client.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    async def get_all_details(self) -> List[AssetDetailT]:
+        all_resources = await self.get_list()
+        detail_tasks = [self.get_detail(resource.url) for resource in all_resources]
+        return await asyncio.gather(*detail_tasks)
+
+    async def get_detail(self, url: str) -> AssetDetailT:
+        response = await self.client.get(url)
+        response.raise_for_status()
+        return self.DETAIL_CLASS(**response.json())
+
+
+class BerryClient(NamedAssetAPIResource[Berry]):
+    ENDPOINT = "berry"
+    DETAIL_CLASS = Berry
