e48e8afea388b02e034eccf634033ce0d6f4545c
Matías González Pazos <matiasggonzalez42@gmail.com>
2025-08-12 00:34:21 +0200

    Move docs

--- app/main.py
+++ app/main.py
@@ -11,9 +11,10 @@ logging.basicConfig(level=settings.logger_level)
 app = FastAPI(
     title="Poké API",
     version="0.1.0",
+    docs_url="/",
 )
 
-app.include_router(berries.router)
+app.include_router(berries.router, tags=["Berry statistics"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
